88e268a375a8d61e2a7b23bf5f4cfe80af248782
Sanjay Upadhyay <saneax@gmail.com>
2025-10-03 05:37:46 +0000

    typo fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-ROOT_DIR="$(cd "$(dirname "$0")" && pwd)
+ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$ROOT_DIR"
 
 echo "=== setup.sh: Starting ($(date)) ==="
